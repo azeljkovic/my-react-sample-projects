e68d5013c462924abb558a9229edd9fa4cb93aa9
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-03-28 13:03:08 +0200

    added page with navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './style.css';
 
-const navbar = (
-  <nav>
-    <h1>Coke</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-)
 
-ReactDOM.render(navbar, document.getElementById("root"));
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img className="nav-logo"
+             src={"./react-logo.png"}
+             alt="React logo."/>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      © 2022 zeljkovic.sh All rights reserved.
+    </footer>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>
+        Reasons I'm excited to learn React
+      </h1>
+      <ol>
+        <li>It's a popular library, so I'll be
+          able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer
+          if I know React
+        </li>
+      </ol>
+    </div>
+
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </div>
+  )
+}
+
+ReactDOM.render(<Page/>, document.getElementById("root"))
